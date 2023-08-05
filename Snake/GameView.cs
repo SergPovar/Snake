f01585d5ba8ba09769f66ae71de4ef694dc14799
SergPovar <130896201+SergPovar@users.noreply.github.com>
2023-08-05 18:53:50 +0400

    Initial commit

---
+++ Snake/GameView.cs
@@ -0,0 +1,108 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Snake;
+
+/// <summary>
+/// Класс отвечает за отрисовку игры.
+/// </summary>
+public class GameView
+{
+    // Настройки отрисовки игры.
+    private GameViewSettings _gameViewSettings;
+
+    // Змейка.
+    private Snake _snake;
+
+    // Игровое поле.
+    private GameBoard _gameBoard;
+
+    // Контроллер еды.
+    private FoodController _foodController;
+
+    // Спрайты объектов
+    private Sprite _wallSprite;
+    private Sprite _backgroundSprite;
+    private Sprite _snakeBodySprite;
+    private Sprite _snakeHeadSprite;
+    private Sprite _foodSprite;
+
+
+    public GameView(GameViewSettings gameViewSettings, GameBoard gameBoard, Snake snake, FoodController foodController)
+    {
+        _gameViewSettings = gameViewSettings;
+        _snake = snake;
+        _foodController = foodController;
+        _gameBoard = gameBoard;
+
+
+        _wallSprite = new Sprite(_gameViewSettings.WallTexture);
+        _backgroundSprite = new Sprite(_gameViewSettings.BackgroundTexture);
+        _foodSprite = new Sprite(_gameViewSettings.FoodTexture);
+        _snakeBodySprite = new Sprite(_gameViewSettings.SnakeBodyTexture);
+        _snakeHeadSprite = new Sprite(_gameViewSettings.SnakeHeadTexture);
+    }
+
+    /// <summary>
+    /// Метод рисует все игровые объекты.
+    /// </summary>
+    public void DrawGameObjects(RenderWindow window)
+    {
+        DrawFood(window);
+        DrawSnake(window);
+    }
+
+    /// <summary>
+    /// Метод рисует карту.
+    /// </summary>
+    public void DrawMap(RenderWindow window)
+    {
+        window.Draw(_backgroundSprite);
+        for (int x = 0; x < _gameBoard.Size.X; x++)
+        {
+            for (int y = 0; y < _gameBoard.Size.Y; y++)
+            {
+                if (x == 0 || x == _gameBoard.Size.X - 1)
+                {
+                    _wallSprite.Position = new Vector2f(x * _gameViewSettings.CellSize, y * _gameViewSettings.CellSize);
+                    window.Draw(_wallSprite);
+                }
+                else if (y == 0 || y == _gameBoard.Size.Y - 1)
+                {
+                    _wallSprite.Position = new Vector2f(x * _gameViewSettings.CellSize, y * _gameViewSettings.CellSize);
+                    window.Draw(_wallSprite);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Метод рисует еду на карте
+    /// </summary>
+    private void DrawFood(RenderWindow window)
+    {
+        // Умножаем позицию еды, на размер ячейки
+        _foodSprite.Position = new Vector2f(_foodController.Food.X * _gameViewSettings.CellSize,
+            _foodController.Food.Y * _gameViewSettings.CellSize);
+        window.Draw(_foodSprite);
+    }
+
+    /// <summary>
+    /// Метод рисует змейку на карте
+    /// </summary>
+    private void DrawSnake(RenderWindow window)
+    {
+        var headCoordinates = _snake.GetPoint(0);
+        _snakeHeadSprite.Position = new Vector2f(headCoordinates.X * _gameViewSettings.CellSize,
+            headCoordinates.Y * _gameViewSettings.CellSize);
+        window.Draw(_snakeHeadSprite);
+        for (int i = 1; i < _snake.GetSize(); i++)
+        {
+            var snakeCoordinates = _snake.GetPoint(i);
+            _snakeBodySprite.Position = new Vector2f(snakeCoordinates.X * _gameViewSettings.CellSize,
+                snakeCoordinates.Y * _gameViewSettings.CellSize);
+            window.Draw(_snakeBodySprite);
+        }
+    }
+}
---
+++ Snake/InputController.cs
@@ -0,0 +1,61 @@
+﻿using SFML.Window;
+
+namespace Snake;
+
+public class InputController
+{
+    private Snake _snake;
+
+    /// <summary>
+    /// Конструктор объекта InputController, который сохраняет ссылку на объект Snake.
+    /// </summary>
+    public InputController(Snake snake)
+    {
+        // Конструктор сохраняет змейку в поле класса.
+        _snake = snake;
+    }
+    
+    public void ProcessInput()
+    {
+        // Проверяем нажатую кнопку и в зависимости от нее - задаем змейке соответствующее направление.
+
+        // Перемещаем змейку вверх
+        if (Keyboard.IsKeyPressed(Keyboard.Key.W) || Keyboard.IsKeyPressed(Keyboard.Key.Up))
+        {
+            // Проверка, что текущее направление змейки не вниз.
+            if ((int)_snake.Direction.Y != 1)
+            {
+                _snake.SetMoveDirectionToUp();
+            }
+        }
+        if (Keyboard.IsKeyPressed(Keyboard.Key.S) || Keyboard.IsKeyPressed(Keyboard.Key.Down))
+        {
+            if ((int)_snake.Direction.Y != -1)
+            {
+                _snake.SetMoveDirectionToDown();
+            }
+        }
+        if (Keyboard.IsKeyPressed(Keyboard.Key.A) || Keyboard.IsKeyPressed(Keyboard.Key.Left))
+        {
+            if ((int)_snake.Direction.X != 1)
+            {
+                _snake.SetMoveDirectionToLeft();
+            }
+        }
+        if (Keyboard.IsKeyPressed(Keyboard.Key.D) || Keyboard.IsKeyPressed(Keyboard.Key.Right))
+        {
+            if ((int)_snake.Direction.X != -1)
+            {
+                _snake.SetMoveDirectionToRight();
+            }
+        }
+        // Двигаем змейку.
+        MoveSnake();
+    }
+
+    public void MoveSnake()
+    {
+        // Двигаем змейку.
+        _snake.MoveForward();
+    }
+}
